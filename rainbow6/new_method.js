dadbb20cc4b72318a682229f6b5e983c85cd4022
IcedDog <34804287+IcedDog@users.noreply.github.com>
2023-07-15 13:44:14 +0800

    Update new_method.js

--- rainbow6/new_method.js
+++ rainbow6/new_method.js
@@ -18,11 +18,29 @@ precision mediump float;
 varying vec2 vTexCoord;
 uniform sampler2D uSampler;
 
+vec3 hsv2rgb(vec3 c) {
+    const vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+vec3 rgb2hsv(vec3 c) {
+    const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + 0.001)), d / (q.x + 0.001), q.x);
+}
+
 void main() {
   vec4 color = texture2D(uSampler, vTexCoord);
   float sum = color.r + color.g + color.b;
   float convertedGray = sum / 3.0;
-  gl_FragColor = vec4(convertedGray, convertedGray, convertedGray, color.a);
+  vec3 transformed = rgb2hsv(vec3(convertedGray,convertedGray,convertedGray));
+  transformed.r = 1.0;
+  vec3 re = hsv2rgb(transformed);
+  gl_FragColor = vec4(re.r - 0.2,re.g - 0.2,re.b - 0.2, color.a);
 }
 `;
 
