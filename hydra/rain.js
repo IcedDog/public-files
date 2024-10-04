f8cfa8e0466643f51efe4164523af2bea1b643d0
IcedDog <34804287+IcedDog@users.noreply.github.com>
2024-10-04 12:43:35 +0800

    Create rain.js

---
+++ hydra/rain.js
@@ -0,0 +1,16 @@
+// original
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//corrupted screensaver
+//by Ritchse
+//instagram.com/ritchse
+
+voronoi(350,0.15)
+  	.modulateScale(osc(8).rotate(Math.sin(time)),.5)
+  	.thresh(.8)
+	.modulateRotate(osc(7),.4)
+	.thresh(.7)
+  	.diff(src(o0).scale(1.8))
+	.modulateScale(osc(2).modulateRotate(o0,.74))
+	.diff(src(o0).rotate(-.001).scrollY(0,-1/199800))
+	.brightness(-.02)
+	.out()
